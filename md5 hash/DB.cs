b80b8029f2773c224e4cc8e5cf2e44a458dc4e2b
Witek0509x <witek.droch@gmail.com>
2017-11-06 22:04:05 +0100

    db and hash

---
+++ md5 hash/DB.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Data.SQLite;
+
+namespace md5_hash
+{
+    class DB
+    {
+        SQLiteConnection m_dbConnection;
+        public DB(string source)
+        {
+            try
+            {
+                m_dbConnection = new SQLiteConnection("Data Source=" + source + ";Version=3;");
+                m_dbConnection.Open();
+            }
+            catch
+            {
+
+            }
+        }
+        public string Compute(string txt)
+        {
+                using (MD5 md5Hash = MD5.Create())
+                {
+                    return GetMd5Hash(md5Hash, txt);
+                }
+            }
+
+        private string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        private bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool VerifyPassword(string login, string password)
+        {
+            string sql;
+            sql = "select password from users where login = '" + login + "'";
+            string respond = Query(sql);
+            using (MD5 md5Hash = MD5.Create())
+            {
+                return VerifyMd5Hash(md5Hash, respond, Compute(password));
+            }    
+        }
+        public void CreateNewUser(string login, string password, string permission)
+        {
+
+        }
+
+
+
+        public string Query(string sql)
+        {
+            try
+            {
+                string wynik = "";
+                int i = 0;
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    while (reader.FieldCount > i)
+                    {
+                        wynik += reader.GetString(i);
+                        wynik += "  ";
+                        i++;
+                    }
+                    i = 0;
+                    wynik += Environment.NewLine;
+                }
+                return wynik;
+            }
+            catch
+            {
+                return "something went wrong";
+            }
+        }
+
+    }
+
+
+}
+
---
+++ md5 hash/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace md5_hash
+{
+    public partial class Form1 : Form
+    {
+        DB db = new DB("db.sqlite");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tekst = textBox1.Text;
+            label1.Text = db.Compute(tekst);
+        }
+    }
+}
